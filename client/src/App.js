190e6e6e4ac42c09031ee9acd2ebdead8d861fb5
Casey <91210043+peicasey@users.noreply.github.com>
2023-05-25 22:28:02 -0500

    favicon changes + accessability widget attempt

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from 'react';
  
 // We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
@@ -10,16 +11,22 @@ import Edit from "./components/edit";
 import Create from "./components/create";
  
 const App = () => {
- return (
-   <div>
-     <Navbar />
-     <Routes>
-       <Route exact path="/" element={<RecordList />} />
-       <Route path="/edit/:id" element={<Edit />} />
-       <Route path="/create" element={<Create />} />
-     </Routes>
-   </div>
- );
+
+  // PRODUCTION SERVER: https://thrive-together.onrender.com" 
+  // DEBUG SERVER:      http://localhost:5050
+  const [s, setS] = useState('https://thrive-together.onrender.com');
+
+
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<RecordList server={s}/>} />
+        <Route path="/edit/:id" element={<Edit server={s}/>} />
+        <Route path="/create" element={<Create server={s}/>} />
+      </Routes>
+    </div>
+  );
 };
  
 export default App;
