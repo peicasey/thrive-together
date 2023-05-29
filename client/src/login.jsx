f5299c43e3c12da716c575204401bf17196353a8
ryankbr <ryan_ashk@outlook.com>
2023-05-29 04:24:22 -0500

    login done, not integrated with front end (3)

--- client/src/login.jsx
+++ client/src/login.jsx
@@ -1,17 +1,17 @@
 // login template code (client id is accurate, copy+paste the same one each time)
 import { useEffect, useState } from 'react';
-import jwt_decode from "jwt-decode";
+import jwt_decode from "jwt-decode"; // to decode json webtokens that we get from a user login via google sign-in
 // global google script added to index.html under ../public
 
 function Login() { // this will essentially create a sign-in button with a sign-out button fully implemented
     const [ user, setUser ] = useState({});
 
     function handleCallbackResponse(response) {
-        console.log("Encoded JWT ID token: " + response.credential);
-        var userObject = jwt_decode(response.credential);
+        console.log("Encoded JWT ID token: " + response.credential); // logs the user's json webtoken for decoding (this is how we get the info on who's who from google)
+        var userObject = jwt_decode(response.credential); // this is from jwt-decode to decode the user data from a json webtoken
         console.log(userObject);
         setUser(userObject);
-        document.getElementById("signInDiv").hidden = true;
+        document.getElementById("signInDiv").hidden = true; // hides sign-in button after a successful login
     } // authorized test sign-in users: casey (gmail, tamu email), ryan (gmail, tamu email)
 
     function handleSignOut(event) { // we don't really need event, this is here incase of additional functionality
@@ -22,7 +22,7 @@ function Login() { // this will essentially create a sign-in button with a sign-
     useEffect(() => {
         /* global google */
         google.accounts.id.initalize({
-            client_id: "892235413140-53t2tbh7i0dbau2vvntgfcam0mg1ordl.apps.googleusercontent.com",
+            client_id: "892235413140-53t2tbh7i0dbau2vvntgfcam0mg1ordl.apps.googleusercontent.com", // our app's unique sign in id authorized for localhost, localhost:3000, and the thrive-together vercel app link 
             callback: handleCallbackResponse
         });
 
